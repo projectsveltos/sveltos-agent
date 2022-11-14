628e044390b71900cd54a397800860139a6327cc
gianlucm76 <gianluca.mardente@gmail.com>
2022-11-14 09:28:37 -0800

    Fix classifier-agent crash

--- main.go
+++ main.go
@@ -119,7 +119,7 @@ func main() {
 	setupChecks(mgr)
 
 	setupLog.Info("starting manager")
-	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
+	if err := mgr.Start(ctx); err != nil {
 		setupLog.Error(err, "problem running manager")
 		os.Exit(1)
 	}
